ee36e3fce7f14fb9bcd07f034ba2075360c37d89
David Rapoport <drapoport847@gmail.com>
2023-02-11 19:33:09 -0700

    Remove old attempt at headless scrape

--- index.js
+++ index.js
@@ -5,9 +5,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import HeadlessScraper from './HeadlessScraper';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerHeadlessTask('HeadlessScraper', () =>
-    require('./HeadlessScraper'),
-);
